75c6f521af3ff782bbafa2a9388cdaafbc567aa9
артем дербин <artem.derbinkpc612020@gmail.com>
2023-10-26 00:22:55 +0300

    Добавил тестик

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
+import io.restassured.http.ContentType;
+import models.requestModel.UserRequestModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static io.restassured.RestAssured.given;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void test() {
+		UserRequestModel user = UserRequestModel.builder()
+				.login("")
+				.password("")
+				.build();
+
+
+		given()
+				.baseUri("http://localhost:8888")
+				.contentType(ContentType.JSON)
+				.body(user)
+				.post("api/data")
+				.then()
+				.log().all()
+				.contentType(ContentType.JSON)
+				.statusCode(200);
+
 	}
 
 }
